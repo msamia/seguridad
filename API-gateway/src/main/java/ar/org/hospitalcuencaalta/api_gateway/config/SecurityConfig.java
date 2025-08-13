ed43f7566e9e49588dded46b3f804eb2f7c46f29
msamia <msamia@gmail.com>
2025-08-13 18:42:38 -0300

    Configuración de seguridad en API-gateway

--- API-gateway/src/main/java/ar/org/hospitalcuencaalta/api_gateway/config/SecurityConfig.java
+++ API-gateway/src/main/java/ar/org/hospitalcuencaalta/api_gateway/config/SecurityConfig.java
@@ -2,26 +2,37 @@ package ar.org.hospitalcuencaalta.api_gateway.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
-import org.springframework.security.oauth2.jwt.ReactiveJwtDecoder;
-import reactor.core.publisher.Mono;
 
 @Configuration
 @EnableWebFluxSecurity
 public class SecurityConfig {
 
+//    @Bean
+//    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+//        http.csrf(ServerHttpSecurity.CsrfSpec::disable);
+//        http.authorizeExchange(exchange -> exchange.anyExchange().authenticated());
+//        http.oauth2ResourceServer(ServerHttpSecurity.OAuth2ResourceServerSpec::jwt);
+//        return http.build();
+//    }
+//
+//    @Bean
+//    public ReactiveJwtDecoder jwtDecoder() {
+//        return token -> Mono.error(new UnsupportedOperationException("No JWT decoder configured"));
+//    }
+
+
     @Bean
     public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
         http.csrf(ServerHttpSecurity.CsrfSpec::disable);
         http.authorizeExchange(exchange -> exchange.anyExchange().authenticated());
-        http.oauth2ResourceServer(ServerHttpSecurity.OAuth2ResourceServerSpec::jwt);
+        http.oauth2ResourceServer(t -> {
+            t.jwt(Customizer.withDefaults());
+        });
         return http.build();
     }
 
-    @Bean
-    public ReactiveJwtDecoder jwtDecoder() {
-        return token -> Mono.error(new UnsupportedOperationException("No JWT decoder configured"));
-    }
 }
