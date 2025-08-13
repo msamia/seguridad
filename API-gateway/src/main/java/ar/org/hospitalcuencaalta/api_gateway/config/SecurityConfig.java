721d8fff0cd4412a86f02b2cea662be74e3a5354
msamia <msamia@gmail.com>
2025-08-13 18:48:39 -0300

    Configure Keycloak JWT decoder

--- API-gateway/src/main/java/ar/org/hospitalcuencaalta/api_gateway/config/SecurityConfig.java
+++ API-gateway/src/main/java/ar/org/hospitalcuencaalta/api_gateway/config/SecurityConfig.java
@@ -2,9 +2,12 @@ package ar.org.hospitalcuencaalta.api_gateway.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.jwt.NimbusReactiveJwtDecoder;
+import org.springframework.security.oauth2.jwt.ReactiveJwtDecoder;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @Configuration
@@ -33,6 +36,11 @@ public class SecurityConfig {
             t.jwt(Customizer.withDefaults());
         });
         return http.build();
+    }
+
+    @Bean
+    public ReactiveJwtDecoder jwtDecoder(@Value("${spring.security.oauth2.resourceserver.jwt.jwk-set-uri}") String jwkSetUri) {
+        return NimbusReactiveJwtDecoder.withJwkSetUri(jwkSetUri).build();
     }
 
 }
